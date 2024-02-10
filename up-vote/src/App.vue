78e88501ff0ace6d3867cdb5db66e075658f0671
suleiman hamza <hamzasuleiman530@gmail.com>
2024-02-10 12:27:58 +0000

    updated and debug files for upvote app

--- up-vote/src/App.vue
+++ up-vote/src/App.vue
@@ -1,47 +1,103 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <h1 class="title">Premire League</h1>
+    <!--single submission component with props passed to child component-->
+    <Submission 
+      v-for="(user, index) in sortedUsers" 
+      :sortedUsers="sortedUsers" 
+      :user="user" 
+      :increase="increase"
+      :submission="submission"
+      :key="index"
+      :class="{active: sortedUsers.votes >= 60}"
+    />
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+import { ref, computed } from 'vue';
+import Submission from './components/Submission.vue';
+
+//initial user data before being sorted
+let users = ref([
+  {
+    id: 1,
+    club: 'Arsenal',
+    coach: 'Mikel Arterta',
+    motto: 'Gunners',
+    votes: 52
+  },
+  {
+    id: 2,
+    club: 'Manchester city',
+    coach: 'Pep Guardiola',
+    motto: 'Sky Blue',
+    votes: 45
+  },
+  {
+    id: 3,
+    club: 'Manchetser United',
+    coach: 'Ten Hag',
+    motto: 'Red Devils',
+    votes: 48
+  },
+  {
+    id: 4,
+    club: 'Liverpool',
+    coach: 'Jurgen klopp',
+    motto: 'YMWA',
+    votes: 43
+  },
+  {
+    id: 5,
+    club: 'Chelsea',
+    coach: 'Ancelotti',
+    motto: 'The Blues',
+    votes: 5
+  }
+])
+
+//sorted user data by votes
+const sortedUsers = computed(() => {
+  return users.value.sort((a, b) => {
+    return b.votes - a.votes 
+  })
+})
+
+//increment button logic, find userid and increase the votes
+const increase = (userid) => {
+  console.log(userid)
+  const submission = users.value.find((sumb) => {
+      return sumb.id === userid
+    });
+  submission.votes++
 }
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+<style>
+h3, p {
+  margin-top: 0;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.title {
+  text-align: center;
+}
+.media {
+  display: flex;
+  align-items: center;
+  gap: 1.5rem;
+  border: 2px solid grey;
+  padding: 1rem;
+}
+.media_icon {
+  display: inline-block;
+  width: 105px;
+  height: 105px;
+  background-color: chocolate;
+}
+.media_votes {
+  margin-left: auto;
+}
+.active {
+  color: red;
 }
 </style>
---
+++ up-vote/src/components/Submission.vue
@@ -0,0 +1,18 @@
+<template>
+    <article class="media">
+      <div class="media_icon"></div>
+      <div class="media_desc">
+        <h3>{{ user.club }}</h3>
+        <p>Coach: {{ user.coach }}</p>
+        <p>{{ user.motto }}</p>
+      </div>
+      <div class="media_votes">
+        <p>{{ user.votes }}</p>
+        <button type="button" v-on:click="increase(user.id)">+</button>
+      </div>
+    </article>
+</template>
+
+<script setup>
+    const props = defineProps(['sortedUsers', 'increase', 'user', 'submission'])
+</script>
